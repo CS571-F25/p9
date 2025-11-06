68b3fb37ee1506e08fb83001b362391acc6f0a1d
Adin Lieblein <adinmlieblein@gmail.com>
2025-11-05 18:08:11 -0600

    project setup

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  base: '/p9/',
+  build: {
+    outDir: 'docs',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
