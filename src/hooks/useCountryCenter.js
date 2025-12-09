cd0ef4a8e08fc85a070008f08aef5831eb240799
Adin Lieblein <adinmlieblein@gmail.com>
2025-12-09 14:42:52 -0600

    https hotfix rebuild

--- src/hooks/useCountryCenter.js
+++ src/hooks/useCountryCenter.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 const fetchCountryCenter = async (iso3) => {
   if (!iso3 || iso3.length !== 3) throw new Error("ISO3 code is required");
-  const response = await fetch(`http://inmagik.github.io/world-countries/countries/${iso3}.json`);
+  const response = await fetch(`https://inmagik.github.io/world-countries/countries/${iso3}.json`);
   const data = await response.json();
   const center = data.representative_point;
   return { lat: center[1], lng: center[0] };
